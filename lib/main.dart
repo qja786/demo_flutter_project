0d1c6ba2ab7a155a507db4bdf32bd1d4b6af222d
Jamal Ahmad <qja786@gmail.com>
2022-08-02 10:09:26 +0530

    Day2 first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo_flutter_project/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -5,18 +6,11 @@ void main(){
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to My first Flutter App"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
