0bb19d42f459dda96655594fac620993face3507
Jamal Ahmad <qja786@gmail.com>
2022-08-02 12:00:28 +0530

    Day2 commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final int days = 31;
+    final int days = 35;
     final String name = "Jamal Ahmad";
     return Scaffold(
       appBar: AppBar(
